76b24fa8baac7896172aca5b341809d3f1381daf
ame <mtk20170119@gmail.com>
2026-06-28 14:28:14 +0800

    change setting,add about,readme

--- wordlist/frontend/src/App.vue
+++ wordlist/frontend/src/App.vue
@@ -13,20 +13,28 @@
         </nav>
 
         <div class="settings-wrap">
-          <button class="btn" type="button" @click="toggleSettings">设置</button>
+          <button class="btn" type="button" @click="toggleSettings">我的</button>
           <div v-if="settingsOpen" class="settings-panel">
-            <h3>用户设置</h3>
-            <label class="field">
-              <span>u_id</span>
-              <input v-model="draftUserId" type="text" placeholder="default" />
-            </label>
-            <div class="actions">
-              <button class="btn btn-primary" type="button" @click="saveUserId">保存</button>
-              <button class="btn" type="button" @click="closeSettings">关闭</button>
+            <h3>我的</h3>
+            <div class="menu-list">
+              <button class="menu-item" type="button" @click="openUserSettings">设置用户</button>
+              <button class="menu-item" type="button" @click="openAboutPage">关于</button>
+              <button class="menu-item" type="button" @click="openProductPage">日语学习平台</button>
             </div>
-            <p v-if="settingsMessage" :class="['muted', settingsMessageType === 'warning' ? 'warning-text' : '']">
-              {{ settingsMessage }}
-            </p>
+
+            <div v-if="showUserSettings" class="user-settings-block">
+              <label class="field">
+                <span>用户 ID</span>
+                <input v-model="draftUserId" type="text" placeholder="例如 demo" />
+              </label>
+              <div class="actions">
+                <button class="btn btn-primary" type="button" @click="saveUserId">保存</button>
+                <button class="btn" type="button" @click="closeUserSettings">取消</button>
+              </div>
+              <p v-if="settingsMessage" :class="['muted', settingsMessageType === 'warning' ? 'warning-text' : '']">
+                {{ settingsMessage }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -44,6 +52,7 @@ import { createUser } from './api/client';
 import { getStoredUserId, normalizeUserId, setStoredUserId, subscribeUserIdChange } from './utils/user';
 
 const settingsOpen = ref(false);
+const showUserSettings = ref(false);
 const draftUserId = ref(getStoredUserId());
 const currentUserId = ref(getStoredUserId());
 const settingsMessage = ref('');
@@ -52,11 +61,13 @@ const settingsMessageType = ref<'info' | 'warning'>('info');
 function openSettings() {
   draftUserId.value = currentUserId.value;
   settingsMessage.value = '';
+  showUserSettings.value = false;
   settingsOpen.value = true;
 }
 
 function closeSettings() {
   settingsOpen.value = false;
+  showUserSettings.value = false;
 }
 
 function toggleSettings() {
@@ -65,6 +76,24 @@ function toggleSettings() {
     return;
   }
   openSettings();
+}
+
+function openUserSettings() {
+  draftUserId.value = currentUserId.value;
+  settingsMessage.value = '';
+  showUserSettings.value = true;
+}
+
+function closeUserSettings() {
+  showUserSettings.value = false;
+}
+
+function openAboutPage() {
+  window.open('https://github.com/Android-9527/englishWord', '_blank', 'noopener,noreferrer');
+}
+
+function openProductPage() {
+  window.open('http://136.117.65.65/', '_blank', 'noopener,noreferrer');
 }
 
 async function saveUserId() {
